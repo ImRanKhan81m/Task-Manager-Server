a0c6bce96ed3b1b19711dc2c41cc4aa8c210592a
Imran Hossen <freelancerimran81@gmail.com>
2022-07-01 04:11:01 +0600

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.815jo.mongodb.net/?retryWrites=true&w=majority`;
+  `mongodb+srv://todoTask:Nlqa7X3SjwaBYR8c@cluster0.815jo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
