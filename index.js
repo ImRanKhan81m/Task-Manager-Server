b7ed1626c72fd260003f6599640332cd0e99db7c
Imran Hossen <freelancerimran81@gmail.com>
2022-07-01 04:25:17 +0600

    updated

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 //todoTask
 //Nlqa7X3SjwaBYR8c
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
